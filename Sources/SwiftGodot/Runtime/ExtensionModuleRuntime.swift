d27791256d54325782b416de0b54efac464344b1
Elijah Semyonov <elijah.semenov@gmail.com>
2024-10-16 05:55:25 +0200

    Snapshot

--- Sources/SwiftGodot/Runtime/ExtensionModuleRuntime.swift
+++ Sources/SwiftGodot/Runtime/ExtensionModuleRuntime.swift
@@ -6,13 +6,17 @@
 //
 
 public class ExtensionModuleRuntime {
-    public static let shared = ExtensionModuleRuntime()
-    
     private var classes: [String: ExtensionClassDescriptor] = [:]
+    
+    public init() {
+        
+    }
     
     public func addClass(decriptedBy descriptor: ExtensionClassDescriptor) {
         if classes[descriptor.name] != nil {
             fatalError("Class named `\(descriptor.name)` is already registered")
         }
+        
+        classes[descriptor.name] = descriptor
     }
 }
